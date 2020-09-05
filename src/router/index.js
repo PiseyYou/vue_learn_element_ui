2e7cd00624a62cc539e8c700410ab621978b1cda
PiseyYou <feng.you@hotmail.com>
2020-09-05 23:09:09 +0800

    rights a bug assign roles is not working

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,13 @@ import Login from '@/components/Login'
 import Home from '../components/Home'
 import Welcome from '@/components/Welcome'
 import Users from '@/components/user/Users'
+import Rights from '@/components/power/Rights'
+import Roles from '@/components/power/Roles'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [
-    {
+  routes: [{
       path: '/',
       redirect: '/login'
     },
@@ -21,14 +22,20 @@ const router = new VueRouter({
       path: '/home',
       component: Home,
       redirect: '/Welcome',
-      children: [
-        {
+      children: [{
           path: '/Welcome',
           component: Welcome
         },
         {
           path: '/users',
           component: Users
+        },
+        {
+          path: '/rights',
+          component: Rights
+        }, {
+          path: '/roles',
+          component: Roles
         }
       ]
     }
