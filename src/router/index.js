4900ae77b6bac1435aa7e89389d15b24663f61fc
PiseyYou <feng.you@hotmail.com>
2020-09-06 21:17:46 +0800

    a bug for many not edit

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Users from '@/components/user/Users'
 import Rights from '@/components/power/Rights'
 import Roles from '@/components/power/Roles'
 import Cate from '@/components/goods/Cate'
+import Params from '@/components/goods/Params'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -40,6 +42,10 @@ const router = new VueRouter({
         {
           path: '/categories',
           component: Cate
+        },
+        {
+          path: '/params',
+          component: Params
         }
       ]
     }
