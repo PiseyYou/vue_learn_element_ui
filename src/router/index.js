f29f1367bf09c2e55940cedd2245aa92c315c11a
PiseyYou <feng.you@hotmail.com>
2020-09-10 10:30:38 +0800

    update bug problem

--- src/router/index.js
+++ src/router/index.js
@@ -8,24 +8,56 @@ import Rights from '@/components/power/Rights'
 import Roles from '@/components/power/Roles'
 import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
+import List from '../components/goods/List'
+import Add from '../components/goods/Add'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
+  routes: [{
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
     {
       path: '/home',
       component: Home,
       redirect: '/Welcome',
-      children: [
-        { path: '/Welcome', component: Welcome },
-        { path: '/users', component: Users },
-        { path: '/rights', component: Rights },
-        { path: '/roles', component: Roles },
-        { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+      children: [{
+          path: '/Welcome',
+          component: Welcome
+        },
+        {
+          path: '/users',
+          component: Users
+        },
+        {
+          path: '/rights',
+          component: Rights
+        },
+        {
+          path: '/roles',
+          component: Roles
+        },
+        {
+          path: '/categories',
+          component: Cate
+        },
+        {
+          path: '/params',
+          component: Params
+        },
+        {
+          path: '/goods',
+          component: List
+        },
+        {
+          path: '/goods/add',
+          component: Add
+        }
       ]
     }
   ]
